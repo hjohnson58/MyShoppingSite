54e70e751ef8c1b13eba3b83ad14f294da09a2bc
hjohnson58 <79508327+hjohnson58@users.noreply.github.com>
2021-03-10 01:34:34 -0600

    Create phpfunctions.php

---
+++ library/phpfunctions.php
@@ -0,0 +1,36 @@
+<?php
+
+function myfooter() {
+        echo "<!--  I honor Parkland's core values by affirming that I have 
+followed all academic integrity guidelines for this work.
+Henry Johnson 
+CSC-155-201F_2021SP -->";
+        echo "<center>";
+        echo "<a href='http://www.csit.parkland.edu/~hjohnson58/csc155labs'>A page</a>";
+        echo "&nbsp;|&nbsp;";
+        echo "<a href=''>Page under Construction</a>";
+        echo "</center>";
+}
+function myheader() {
+        echo "<center>";
+        echo "<a href='http://www.csit.parkland.edu/~hjohnson58/csc155labs/php/phpsites/login.php'>Login</a>";
+        echo "&nbsp;|&nbsp;";
+        echo "<a href='http://www.csit.parkland.edu/~hjohnson58/csc155labs/php/phpsites/welcome.php'>Welcome</a>";
+        echo "&nbsp;|&nbsp;";
+        echo "<a href='http://www.csit.parkland.edu/~hjohnson58/csc155labs/php/phpsites/purchase1.php'>Purchase1</a>";
+        echo "&nbsp;|&nbsp;";
+        echo "<a href='http://www.csit.parkland.edu/~hjohnson58/csc155labs/php/phpsites/purchase2.php'>Purchase2</a>";
+        echo "&nbsp;|&nbsp;";
+        echo "<a href='http://www.csit.parkland.edu/~hjohnson58/csc155labs/php/phpsites/purchase3.php'>Purchase3</a>";
+        echo "&nbsp;|&nbsp;";
+        echo "<a href='http://www.csit.parkland.edu/~hjohnson58/csc155labs/php/phpsites/purchase4.php'>Purchase4</a>";
+        echo "&nbsp;|&nbsp;";
+        echo "<a href='http://www.csit.parkland.edu/~hjohnson58/csc155labs/php/phpsites/shoppingcart.php'>Shopping Cart</a>";
+        echo "&nbsp;|&nbsp;";
+        echo "<a href='http://www.csit.parkland.edu/~hjohnson58/csc155labs/php/phpsites/logout.php'>Logout</a>";
+        echo "&nbsp;|&nbsp;";
+        echo "<a href=''>Page under Construction</a>";
+        echo "</center>";
+}
+
+?>
